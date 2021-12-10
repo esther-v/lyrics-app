bbb2566a1cd48fac42ad328d8e5316f516f86a83
esther-v <esvalas@gmail.com>
2021-12-10 18:40:54 +0100

    get data ✅

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-const base_url = 'https://api.lyrics.ovh/v1/'
+const base_url = 'https://api.lyrics.ovh/'
+
+const searchBtn = document.querySelector('#search')
+const form = document.querySelector('#form')
+
+
+//get search value
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    searchValue = searchBtn.value.trim()
+
+    if(!searchValue) {
+        alert("Please enter a value")
+    } else {
+        searchSongs(searchValue)
+    }
+})
+
+//function search
+async function searchSongs(searchValue) {
+    const searchResults = await fetch(`${base_url}/suggest/${searchValue}`)
+    const data = await searchResults.json()
+
+    console.log(data)
+}
