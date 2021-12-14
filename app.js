1f04f2964662d4dfa78b24c4ff24a3b197fa81cd
esther-v <esvalas@gmail.com>
2021-12-14 13:21:45 +0100

    get lyrics and style results

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const base_url = 'https://api.lyrics.ovh/'
+const base_url = 'https://api.lyrics.ovh'
 
 const searchBtn = document.querySelector('#search')
 const form = document.querySelector('#form')
 const results = document.querySelector(".results")
+const titlePage = document.querySelector("h1")
+
+//reset all
+titlePage.addEventListener('click', () => {
+    results.innerHTML = ''
+    searchBtn.value = ''
+})
 
 
 //get search value
@@ -51,6 +58,21 @@ results.addEventListener('click', e => {
     if(clickedBtn.tagName === 'BUTTON') {
         const artist = clickedBtn.getAttribute('data-artist')
         const songTitle = clickedBtn.getAttribute('data-songtitle')
-        console.log(artist, songTitle)
+        
+        getLyrics(artist, songTitle)
     }
 })
+
+//get and display lyrics
+async function getLyrics(artist, songTitle) {
+    const response = await fetch(`${base_url}/v1/${artist}/${songTitle}`)
+
+    const data = await response.json()
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+
+    results.innerHTML = `
+        <h2><strong>${artist}</strong> - ${songTitle}</h2>
+        <p style="text-align:center;">${lyrics}</p>
+    `
+}
